7842f097fa6c7ec2b872bab317d7f97a4e9a3eb8
rall089 <ronnielallman@gmail.com>
2026-03-03 18:19:52 -0800

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,7 @@
+// ─────────────────────────────────────────────
+// THE ONLY FILE YOU NEED TO EDIT
+// Paste your two Supabase values below
+// ─────────────────────────────────────────────
+
+window.SUPABASE_URL = https://qvjmnjbqkstazzmcoodd.supabase.co;
+window.SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2am1uamJxa3N0YXp6bWNvb2RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1ODEzNDMsImV4cCI6MjA4ODE1NzM0M30.wFKn06dsgkLFRXTIJCMsu3Q_9_N54Yhb23oLn6XU51o;
